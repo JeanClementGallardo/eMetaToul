a66552261248b852db244a8eabb4d254c1d2e385
JeanClement <jeanclement.gallardo@inrae.fr>
2024-07-19 15:13:29 +0200

    try baseURL config

--- plugins/globalDirectives.ts
+++ plugins/globalDirectives.ts
@@ -3,4 +3,6 @@ import clickOutside from '../directives/clickOutside';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('click-outside', clickOutside);
+
+  const baseURL = '/eMetaToul';
 });
